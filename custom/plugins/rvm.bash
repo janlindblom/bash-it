ec9fb1e51fdbd2560ec0fa722158987f6094caac
Eric Rochester <err8n@virginia.edu>
2012-10-08 15:45:10 -0400

    Update rvmgo function with better version.

--- custom/plugins/rvm.bash
+++ custom/plugins/rvm.bash
@@ -1,24 +1,10 @@
-# From https://gist.github.com/3498428
+# From https://gist.github.com/3505088
 
 function rvmgo {
-  ver=1.9.3
-  ver=${1-$ver}
+    typeset ver pwd
+    ver=${1:-1.9.3}
+    pwd=${PWD##*/}
 
-  pwd=${PWD##*/}
-
-
-
-  rvm $ver
-  if [ $? == 0 ]; then
-    # rvm to a non existing version will set $? to 1. However rvm to a non existing gemset in an existing version
-    # returns 0 and an onscreen error - so we need to check for the gemset another way
-    rvm $ver@$pwd
-    gemdir=$(rvm gemset dir)
-    if grep -q $pwd <<<$gemdir; then
-      echo "Changed to $ver@$pwd"
-    else
-      rvm gemset create $pwd
-      rvm $ver@$pwd
-    fi
-  fi
+    rvm use $ver@$pwd --create
 }
+
